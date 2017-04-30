c1672a0ce9fb7a1869132eec35af2e1687571ff3
Cashiuus <cashiuus@gmail.com>
2017-04-30 11:37:37 -0400

    Removing files not needed

--- kali-scripts/launch-2terminals.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gnome-terminal -t "Nmap" --hide-menubar --geometry=70x25+30+0 --working-directory=/usr/share/nmap/scripts
-
-gnome-terminal -t "Pentest" --window --hide-menubar --geometry=70x25+30+400 --working-directory=/opt
-
--- kali-scripts/launch-3terminals.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gnome-terminal -t "Pentest" --window --hide-menubar --geometry=82x20+20+0 --working-directory=/opt
-
-gnome-terminal --window --hide-menubar --geometry=82x20+20+400
-
-gnome-terminal -t "Nmap" --hide-menubar --geometry=80x50+800+20 --working-directory=/usr/share/nmap/scripts
